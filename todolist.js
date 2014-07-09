a913ca74d98f861616526a17ff84c62a2833a8e9
yongwoo jeon <i.nevalose@gmail.com>
2014-07-09 23:51:19 +0900

    2weeks

--- todolist.js
+++ todolist.js
@@ -10,12 +10,14 @@ function makeTODO(todo){
 	var input = document.createElement("input");
 	input.className = "toggle";
 	input.setAttribute("type","checkbox");
+	input.addEventListener("click",completedTODO);
 
 	var label = document.createElement("label");
 	label.innerText = todo;
 
 	var button = document.createElement("button");
 	button.className = "destroy";
+	button.addEventListener("click",removeTODO);
 
 	div.appendChild(input);
 	div.appendChild(label);
@@ -26,11 +28,50 @@ function makeTODO(todo){
 	return li;
 }
 
+function completedTODO(e){
+	var input = e.currentTarget;
+	var li = input.parentNode.parentNode;
+	if(input.checked){
+		li.className = "completed";
+
+	}else{
+		li.className = "";		
+	}
+}
+
+function removeTODO(e){
+	var li = e.currentTarget.parentNode.parentNode;
+	var i = 0;
+	var key = setInterval(function(){
+		if(i===50){
+			clearInterval(key);
+			li.parentNode.removeChild(li);
+		}else{
+			li.style.opacity = 1 - i*0.02;
+		}
+		i++;
+	},16);
+
+}
+
 
 function addTODO(e){
 	if(e.keyCode === ENTER_KEYCODE){
 		var todo = makeTODO(document.getElementById("new-todo").value);
+		todo.style.opacity = 0;
 		document.getElementById("todo-list").appendChild(todo);
+
+		var i = 0;
+		var key = setInterval(function(){
+			if(i===50){
+				clearInterval(key);
+			}else{
+				todo.style.opacity = i*0.02;
+			}
+			i++;
+		},16);
+
+
 		document.getElementById("new-todo").value = "";
 
 	}
@@ -39,3 +80,6 @@ function addTODO(e){
 document.addEventListener("DOMContentLoaded",function(){
 	document.getElementById("new-todo").addEventListener("keydown",addTODO);
 });
+
+
+

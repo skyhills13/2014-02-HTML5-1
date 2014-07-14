6164edc60012b0427d66816832a2a7ae05239696
skyhills13 <skyhills13@gmail.com>
2014-07-14 18:14:55 +0900

    [week2-pre] 1. 등록한 할일을 완료처리하기

--- todolist.js
+++ todolist.js
@@ -1,7 +1,14 @@
-// # 첫번째
-// 1. 할일에 넣고 엔터를 치면 할일이 추가된다.
-// 	- 이벤트를 할당한다.(addEventListener)
-// 	- 할일을 추가한다.(appendChild,insertAdjacentHTML)
+/*
+# 두번째
+1. 등록한 할일을 완료처리하기.
+	- 이벤트 할당하기.
+	- class추가하기(li에 completed)
+2. 삭제하기
+	- 이벤트 할당하기.
+	- li을 서서히 사라지게 처리한 후 삭제.
+3. 등록하기
+	- 애니메이션 기능을 추가.
+*/
 
 var ENTER_KEYCODE = 13;
 
@@ -10,38 +17,28 @@ function addToList(e){
 		var eNewTodo = document.getElementById("new-todo");
 		var eTodoList = document.getElementById("todo-list");
 
-		//var template = document.getElementById("template");
-		//var templateInnerHTML = template.innerHTML;
-		//var compiled = _.template(templateInnerHTML);
 		var compiled = _.template(
 			"<li><div class = \"view\"><input class=\"toggle\" type=\"checkbox\"><label><%=TODO%></label><button class=\"destroy\"></button></div></li>")
 		var result = compiled({TODO:eNewTodo.value});
 		eTodoList.insertAdjacentHTML("beforeend", result);
+		
 		eNewTodo.value ="";
 	}
 }
 
+function checkTodo(e){
+	if(e.target.type === "checkbox"){
+		if(e.target.parentNode.parentNode.className === ""){
+			e.target.parentNode.parentNode.className = "completed";
+		} else {
+			e.target.parentNode.parentNode.className = "";
+		}
+	}
+}
+
 function loadEvent (){
-	document.getElementById("new-todo").addEventListener("keydown",addToList);	
+	document.getElementById("new-todo").addEventListener("keydown",addToList);
+	document.querySelector("ul").addEventListener("click", checkTodo);
 }
 
 document.addEventListener("DOMContentLoaded", loadEvent);
-
-
-/* <html file에 <script type="text/template">로 format 만들어놓는 방법 */
-	/* 아래 코드를 html file의 ul 태그 하위에 삽입 
-	<script type = "text/template" id = "template">
-		<li>
-			<div class = "view">
-				<input class="toggle" type="checkbox">
-				<label><%=TODO%></label>
-				<button class="destroy"></button>
-			</div>
-		</li>
-	</script>
-	*/
-	/*	js file의 아래 부분 주석 해제 
-		var template = document.getElementById("template");
-		var templateInnerHTML = template.innerHTML;
-		var compiled = _.template(templateInnerHTML);
-	*/

9d25d620decc9382a098549a7055502911ad26e2
skyhills13 <skyhills13@gmail.com>
2014-08-04 22:51:20 +0900

    [week3-after] webkitAnimationEnd 이벤트 추가

--- todolist.js
+++ todolist.js
@@ -25,8 +25,6 @@
 ] 
 */
 
-//send의 param이 없는 애끼리 묶을 것인가 
-//url이 같은 애들끼리 묶을 것인가
 var oAjax = {
 	xhr : function(oParam, method, url, callback){
 		var sRequestParam = "";
@@ -56,8 +54,13 @@ var oTodoList = {
 	sRequestUrl : "http://ui.nhnnext.org:3333/skyhills13",
 
 	init : function () {
-		document.addEventListener("DOMContentLoaded", this.addEvents.bind(this));
-		document.addEventListener("DOMContentLoaded", this.loadTodo.bind(this));
+		//DomContentLoaded에는 하나만 붙이는거야. 
+		document.addEventListener("DOMContentLoaded", this.setEnvironments.bind(this));
+	},
+
+	setEnvironments : function(){
+		this.addEvents();
+		this.loadTodo();
 	},
 
 	addEvents: function(){
@@ -65,27 +68,28 @@ var oTodoList = {
 		this.eNewTodo = document.getElementById("new-todo");
 		this.eTodoList = document.getElementById("todo-list"); */
 		this.eNewTodo.addEventListener("keydown",this.addToList.bind(this));
-		this.eTodoList.addEventListener("click", this.checkTodo.bind(this));		
-	},
-
-	insertElement : function(todo, nKey){
-		var compiled = _.template(
-			"<li class =\"created\"><div class = \"view\"><input class=\"toggle\" type=\"checkbox\"><label><%=TODO%></label><button class=\"destroy\"></button></div></li>")
-		var result = compiled({TODO:todo});
-		this.eTodoList.insertAdjacentHTML("beforeend", result);
-		this.eTodoList.lastChild.dataset.key = nKey;
+		this.eTodoList.addEventListener("click", this.delegateClick.bind(this));
 	},
 
 	//TODO:새로고침했을 때 순서가 엉망으로 들어가는 문제 
 	loadTodo : function(){
-
+		var savedList ="";
 		var callback = function(oResult){
 			for(var i = 0; i < oResult.length ; ++i) {
 				if ( oResult[i].nickname === "skyhills13" ) {
-					//console.log(oResult[i].id);
-					this.insertElement(oResult[i].todo, oResult[i].id);
+					var compiled = _.template(
+						"<li class =\"created\" data-key = <%=DATAKEY%> >"
+						+	"<div class = \"view\">"
+						+		"<input class=\"toggle\" type=\"checkbox\">"
+						+		"<label><%=TODO%></label>"
+						+		"<button class=\"destroy\"></button>"
+						+	"</div>"
+						+"</li>")
+					var result = compiled({DATAKEY:oResult[i].id, TODO:oResult[i].todo});
+					savedList += result;
 				} 
 			}
+			this.eTodoList.insertAdjacentHTML("beforeend", savedList);
 		}.bind(this);
 		oAjax.xhr ( null, "GET", this.sRequestUrl, callback);
 
@@ -93,16 +97,22 @@ var oTodoList = {
 
 	addToList : function(e) {
 		if ( e.keyCode == this.ENTER_KEYCODE ) {
-			/* 여기서 이것을 callback으로 사용하는 이유는, ajax가 비동기로 실행되기 때문.
-			callback함수 내부에 있는 내용이, ajax와 별도로 실행되어서, ajax는 실패했는데 
-			dom(겉모습)은 생기는 경우가 있을 수 있기 때문  
-			*/
 			var oParam = {
 				"todo" : this.eNewTodo.value
 			}
 			var callback = function(oResult) {
 				/*javascript로 실행시 li class의 created 지울 것 */
-				this.insertElement(this.eNewTodo.value, oResult.insertId);
+				var compiled = _.template(
+					"<li class =\"created\">"
+					+ 	"<div class = \"view\">"
+					+ 		"<input class=\"toggle\" type=\"checkbox\">"
+					+ 		"<label><%=TODO%></label>"
+					+ 		"<button class=\"destroy\"></button>"
+					+	"</div>"
+					+"</li>")
+				var result = compiled({TODO:this.eNewTodo.value});
+				this.eTodoList.insertAdjacentHTML("beforeend", result);
+				this.eTodoList.lastChild.dataset.key = oResult.insertId;
 				//this.fadeEffect(this.eTodoList.lastChild, 300, 1);
 				this.eNewTodo.value ="";	
 			}.bind(this);
@@ -110,7 +120,7 @@ var oTodoList = {
 		}
 	},
 
-	checkTodo : function(e) {
+	delegateClick : function(e) {
 		var eInput = e.target;
 		var eLi = eInput.parentNode.parentNode;
 		var completed = eInput.checked?"1":"0";
@@ -119,34 +129,43 @@ var oTodoList = {
 		var oParam = {
 			"key" : eLi.dataset.key,
 			"completed" : completed 
+		};
+
+		if( sNodeNameInput == "INPUT") {
+			this.checkTodo(eLi, oParam, completed);
+		} else if (sNodeNameButton == "BUTTON") {
+			this.deleteTodo(eLi, oParam);
+		} else {
+			return;
 		}
-		if(sNodeNameInput == "INPUT" ) {
-			var callback = function(){
-				if(completed === "1"){
-					eLi.className = "completed";
-				} else {
-					eLi.className = "";
-				}
-			}
-			oAjax.xhr(oParam, "POST", this.sRequestUrl+"/"+oParam.key, callback);	
- 		} else if( sNodeNameButton == "BUTTON" ){
- 			var callback = function(){
- 				/*javascript로 fadeEffect사용시, 아래의 코드 5줄은 주석처리후 fadeEffect 주석 해제*/
-				eLi.className = "old";
-				setTimeout(function(){ 
-					eLi.style.display = "none";
-					eLi.parentNode.removeChild(eLi);
-				}, 400);
-				//this.fadeEffect(eLi, 200, -1);		
- 			}
- 			oAjax.xhr(oParam, "DELETE", this.sRequestUrl+"/"+oParam.key, callback);
-		}	
 	},
+	deleteTodo: function(eLi, oParam){
+		var callback = function(){
+			/*javascript로 fadeEffect사용시, 아래의 코드 5줄은 주석처리후 fadeEffect 주석 해제*/
+			eLi.className = "old";
+			var removeElement = function(){
+				eLi.parentNode.removeChild(eLi);
+			};
+			eLi.addEventListener("webkitAnimationEnd", removeElement ,false);
+			};
+		oAjax.xhr(oParam, "DELETE", this.sRequestUrl+"/"+oParam.key, callback);
+	},
+
+	checkTodo : function(eLi, oParam, completed) {
+		var callback = function(){
+			if(completed === "1"){
+				eLi.className = "completed";
+			} else {
+				eLi.className = "";
+			}
+		};
+		oAjax.xhr(oParam, "POST", this.sRequestUrl+"/"+oParam.key, callback);	
+	},
+
 	fadeEffect : function(element, totalTime, direction){
 		var opacity = 0.5 - (direction * 0.5);
 		var interval = 10;
 		var gap = interval / totalTime * direction;	
-
 		var execute = function (nDirection, func) {
 			opacity += gap;
 			element.style.opacity = opacity;
@@ -156,8 +175,7 @@ var oTodoList = {
 					func();
 				}	
 			}
-		}
-
+		};
 		var fading = window.setInterval(execute.bind(this, direction, function(){
 			element.style.display = "none";
 			element.parentNode.removeChild(element);
